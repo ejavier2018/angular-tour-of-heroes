5ccb845a680af83eceabcaf6911a8ae9b6127afb
Edward Javier <edward.javier@gmail.com>
2018-09-30 04:06:19 +0800

    angular-tour-of-heroes

---
+++ src/app/hero.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  constructor(private http: HttpClient, 
+    private messageService: MessageService) { }
+
+  getHeroes():Observable<Hero[]>{
+   // this.messageService.add('HeroService: Fetched Heroes');
+    //return of(HEROES);
+   //console.log(this.http.get<Hero[]>(this.heroesUrl));
+   return this.http.get<Hero[]>(this.heroesUrl);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+
+  private log(message: string){
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+}
